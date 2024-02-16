7aecf9b27303cf2834d77087687b8e16a2299a79
Vikas7512 <guptavikas5375@gmail.com>
2024-02-16 23:45:17 +0530

    backend

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
-
+const cors = require("cors");
 const express = require("express");
 const UserRouter = require("./routes");
 const app = express();
 app.use(express.json());
-
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 const connection = mongoose
   .connect("mongodb://127.0.0.1:27017/library")
   .then(() => console.log(" Backend is Connected!"));
@@ -18,5 +22,3 @@ app.use("/users", UserRouter);
 app.listen(3000);
 
 module.exports = connection;
-
-module;
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ const User = require("./model/usermodel");
 
 const UserRouter = express.Router();
 
-UserRouter.post("/", async (req, res) => {
+UserRouter.get("/", async (req, res) => {
   const { name, email, password, phone_no, stream } = req.body;
   const user = await User.create({ name, email, password, phone_no, stream });
   return res.status(201).json(user);
